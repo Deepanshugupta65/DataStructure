da1b099b6bffab26b66141de729ebe1c4b18ba2b
Deepanshugupta65 <dj911743@gmail.com>
2024-11-26 12:21:28 +0530

    heapify algorithm

--- topics/heap/heap.cpp
+++ topics/heap/heap.cpp
@@ -63,6 +63,23 @@ class heap{
         }cout<<endl;
     }
 };
+// heapify algorithm  
+void heapify(int arr[],int n ,int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i+1;
+    
+    if(left<=n && arr[largest]<arr[left]){
+        largest = left;
+    }
+    if(right <= n && arr[largest]<arr[right]){
+        largest = right;
+    }
+    if(largest != i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
 int main(){
     heap h;
     h.insert(60);
@@ -74,5 +91,17 @@ int main(){
     h.print();
     h.deletefromheap();
     h.print();
+    // int arr[] = {-1,54,53,55,52,50};
+    // int n =5;
+    int arr[] = {-1,54,52,50,60,65,70,90};
+
+    int n = 7;
+    for(int i = n/2;i>0;i--){
+      heapify(arr,n,i);
+    }
+    cout<<"printing the array now in heapify "<<endl;
+    for(int i =1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
     return 0;
 }
